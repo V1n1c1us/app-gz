8bd91e73dada4335b1edd994e8effcfba8380770
V1n1c1us <v.f.diehl@gmail.com>
2019-06-05 01:35:29 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">HOMEEE</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">ABOUT</router-link>
     </div>
     <router-view/>
   </div>
